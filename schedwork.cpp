7b0d241cb74ceac40e31674c2e02eeeaa830e81a
Sanjana Paye <paye@usc.edu>
2023-04-08 06:43:36 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool solveHelper(DailySchedule& sched,const AvailabilityMatrix& avail, size_t dailyNeed, size_t numdays, size_t numworkers, std::vector<size_t>& shiftcounts, int day, int worker, size_t maxShifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +39,54 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t numworkers = avail[0].size();
 
+    size_t numdays = avail.size();
+    
+    std::vector<size_t> shiftcounts;
+    int day=0;
+    int worker = 0;
+    for(int i = 0; i< numworkers; ++i){
+        shiftcounts.push_back(0);
+    } 
+    sched.resize(numdays);
+    for(int j = 0; j < numdays; ++j){
+        sched[j].resize(dailyNeed);
+    }
+
+    return solveHelper(sched, avail, dailyNeed, numdays, numworkers, shiftcounts, day, worker, maxShifts);
 
 
 }
+
+
+bool solveHelper(DailySchedule& sched,const AvailabilityMatrix& avail, size_t dailyNeed, size_t numdays, size_t numworkers, std::vector<size_t>& shiftcounts, int day, int worker, size_t maxShifts){
+    
+    if (worker == dailyNeed){
+        day ++;
+        worker = 0;
+    }
+    if(day == numdays){
+        return true;
+    }
+    
+    for(size_t i = worker; i < numworkers; ++i){
+        if(avail[day][i] == 1){
+            if(shiftcounts[i]< maxShifts){
+                sched[day][worker] = i;
+                ++shiftcounts[i];
+                if(solveHelper(sched, avail, dailyNeed, numdays, numworkers, shiftcounts, day, worker +1, maxShifts)){
+                    return true;
+                }
+                sched[day][worker] = -1;
+                shiftcounts[i]--;
+                
+            }
+        }
+    }
+    return false;
+
+    
+}
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordoptions(const std::set<std::string>& dict, const std::string& in, const std::string& floating, int length, int position, std::string& current, std::set<std::string>& options, int numfloating);
+int countletters(const std::string& in);
+int findfloaters(const std::string& in, int numletters, const std::string& floating);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,79 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    int lengthofword = countletters(in);
+    int numfloating = countletters(floating);
+
+   int position = 0;
+   std::set<std::string> options;
+   std::string current = "";
+   wordoptions(dict, in, floating,lengthofword, position, current, options, numfloating);
+   return options;
 
 }
 
 // Define any helper functions here
+int countletters(const std::string& in){
+   int numletters = 0;
+   while(in[numletters] != '\0'){
+     ++numletters;
+   }
+   return numletters;
+}
+
+
+int findfloaters(const std::string& in, int numletters, const std::string& floating){
+    int numfloats = 0;
+    std::map<char, int> frequencies;
+    for(char c : floating){
+        frequencies[c]++;
+        // if(frequencies.find(c) == frequencies.end()){
+        //     frequencies.insert({c, 1});
+        // }else{
+        //     ++frequencies.find(c)->second;
+        // }
+    }
+    for(char c:in){
+        if(frequencies.find(c)!=frequencies.end() &&(frequencies.find(c)->second>0) ){
+          ++numfloats;
+           --frequencies.find(c)->second;
+        }
+    }
+    return numfloats;
+
+}
+
+
+void wordoptions(const std::set<std::string>& dict, const std::string& in, const std::string& floating, int length, int position, std::string& current, std::set<std::string>& options, int numfloating){
+   if(position == length){
+       int numletters = countletters(current);
+       int numfloaters = findfloaters(current, numletters, floating);
+       if(numfloaters == numfloating && dict.count(current)){
+           //if (dict.find(current)!= dict.end()){
+           options.insert(current);
+           // }
+           //options.insert(current);
+       }
+       return;
+   }else{
+       int numletters = countletters(current);
+       int numfloaters = findfloaters(current, numletters, floating);
+       if((length-numletters)<(numfloating-numfloaters)){
+           return;
+       }
+       if(dict.lower_bound(current) == dict.end() || dict.lower_bound(current)->compare(0, numletters, current) != 0){
+           return;
+       }
+   }
+   if(in[position] != '-'){
+       current.push_back(in[position]);
+       wordoptions(dict, in, floating, length, position+1, current, options, numfloating);
+       current.pop_back();
+   }else{
+       for(char c='a'; c <= 'z'; c++){
+           current.push_back(c);
+           wordoptions(dict, in, floating, length, position+1, current, options, numfloating);
+           current.pop_back();
+       }
+   }
+}
